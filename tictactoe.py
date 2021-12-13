7c914c1e4df2253a46583843e305104855c67775
Vanguardian17 <40181370+Vanguardian17@users.noreply.github.com>
2021-12-13 19:46:53 +0100

    added output

---
+++ tictactoe.py
@@ -0,0 +1,9 @@
+field = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def showField():
+    print('\n', field[1], field[2], field[3], '\n', field[4], field[5],
+          field[6], '\n', field[7], field[8], field[9], '\n',)
+
+
+showField()
