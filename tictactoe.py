21e8937cb4a844a6352beab6432e2c680072335b
Vanguardian17 <40181370+Vanguardian17@users.noreply.github.com>
2021-12-13 19:56:44 +0100

    updated WinCheck()

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,4 @@
+# 0 at the beginning to make using indexed easier
 field = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 
@@ -15,22 +16,35 @@ def placeO(n):
 
 
 def winCheck():
+    gameOver = False
     if(field[1] == field[2] == field[3]):
         print("Player ", field[1], " won the game.")
+        gameOver = True
     if(field[4] == field[5] == field[6]):
         print("Player ", field[4], " won the game.")
+        gameOver = True
     if(field[7] == field[8] == field[9]):
         print("Player ", field[7], " won the game.")
+        gameOver = True
     if(field[1] == field[5] == field[9]):
         print("Player ", field[1], " won the game.")
+        gameOver = True
     if(field[3] == field[5] == field[7]):
         print("Player ", field[3], " won the game.")
+        gameOver = True
     if(field[1] == field[4] == field[7]):
         print("Player ", field[1], " won the game.")
+        gameOver = True
     if(field[2] == field[5] == field[8]):
         print("Player ", field[2], " won the game.")
+        gameOver = True
     if(field[3] == field[6] == field[9]):
         print("Player ", field[3], " won the game.")
+        gameOver = True
+    if(gameOver):
+        return True
+    else:
+        return False
 
 
 showField()
